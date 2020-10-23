ff82772cd02a0d272162d61a0a6cc3b1bb9e6745
ico4e <70281513+ico4e@users.noreply.github.com>
2020-10-23 00:53:36 -0400

    nextjs-platzi-avocado lesson 13

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout: React.FC = ({ children }) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+    return (
+        <header>
+            <p>header</p>
+        </header>
+    )
+}
